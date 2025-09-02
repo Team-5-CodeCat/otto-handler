68cf8f1cd874adf36b181830d2490f41ee22fb4c
saroro <211804464+52galuser@users.noreply.github.com>
2025-09-02 20:18:47 +0900

    feat: Add fastify type

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -1,6 +1,6 @@
 import type { FastifyReply } from 'fastify';
 
-interface ExtendedFastifyReply extends FastifyReply {
+export interface ExtendedFastifyReply extends FastifyReply {
   setCookie: (name: string, value: string, options?: any) => FastifyReply;
   clearCookie: (name: string, options?: any) => FastifyReply;
   cookies: Record<string, string>;
