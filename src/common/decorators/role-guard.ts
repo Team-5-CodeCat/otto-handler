11a20f280b7e8dd8c704a2af83d7c9c1d62410bd
saroro <211804464+52galuser@users.noreply.github.com>
2025-09-03 15:42:14 +0900

    refactor: Rename auth decorator

--- src/common/decorators/role-guard.ts
+++ src/common/decorators/role-guard.ts
@@ -1,13 +1,13 @@
 // roles.decorator.ts
 import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
 import { MemberRole } from '@prisma/client';
-import { AuthGuard } from '../guards/auth.guard';
+import { AuthGuardRole } from '../guards/auth-guard-role.service';
 
 export const ROLES_KEY = Symbol('roles');
 export const Roles = (...roles: MemberRole[]) => SetMetadata(ROLES_KEY, roles);
 
-export const UserRole = (roles?: MemberRole | MemberRole[]) => {
-  const decorators = [UseGuards(AuthGuard)];
+export const AuthGuard = (roles?: MemberRole | MemberRole[]) => {
+  const decorators = [UseGuards(AuthGuardRole)];
 
   if (roles) {
     const roleArray = Array.isArray(roles) ? roles : [roles];
