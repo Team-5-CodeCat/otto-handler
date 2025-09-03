f9076594d55d6802c6d93114d38a4b2973cdd3c1
saroro <211804464+52galuser@users.noreply.github.com>
2025-09-03 15:30:05 +0900

    chore: Add error messages

--- src/auth/constants/auth-error.enum.ts
+++ src/auth/constants/auth-error.enum.ts
@@ -3,4 +3,6 @@ export enum AuthErrorEnum {
   GITHUB_USER = '깃허브로 로그인 해주세요',
   REFRESH_FAIL = '세션이 만료되었어요. 다시 로그인 해주세요',
   EMAIL_ALREADY_EXISTS = '이미 가입된 이메일입니다',
+  NOT_VALID_USER = '로그인이 필요합니다',
+  NOT_VALID_ROLE = '권한이 필요합니다',
 }
