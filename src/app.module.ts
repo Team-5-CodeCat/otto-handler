cfd8605a8b79189b610e1d1583d1897ad402d58c
kimkimkimbo <booooa.kim@gmail.com>
2025-09-05 23:33:49 +0900

    feat: add signup to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
-import { GithubModule } from './integrations/github/github.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     PrismaModule,
     AuthModule,
-    GithubModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
--- src/integrations/github/dtos/request/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './register-github-app.dto';
-export * from './select-repository.dto';
-export * from './select-branch.dto';
