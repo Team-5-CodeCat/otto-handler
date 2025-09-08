633626d35aa9dd990a0506b16f8a66d8272c74d1
kimkimkimbo <booooa.kim@gmail.com>
2025-09-08 13:02:23 +0000

    refactor: auth.module에 필요없는 import 삭제

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,9 @@ import { Global, Module } from '@nestjs/common';
 import { AuthService } from './services';
 import { AuthController } from './controllers';
 import { JwtService } from './services';
-import { ProjectsModule } from '../projects/projects.module';
+
 @Global()
 @Module({
-  imports: [ProjectsModule],
   exports: [JwtService],
   providers: [AuthService, JwtService],
   controllers: [AuthController],
