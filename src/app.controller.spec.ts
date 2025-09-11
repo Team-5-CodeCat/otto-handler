440b9e41b3deee4e67f77f9fad7806d53e9e46e1
Jinwoo Han <qew8502@gmail.com>
2025-09-11 11:12:52 +0000

    fix: test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,14 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RedisService } from '@liaoliaots/nestjs-redis';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
+    const mockRedisService = {
+      getOrThrow: jest.fn().mockReturnValue({
+        ping: jest.fn().mockResolvedValue('PONG'),
+      }),
+    };
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: RedisService,
+          useValue: mockRedisService,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
